7b97c8b5a5eb9eadc767a74491ef133bc34742d0
melissa.forfitt <melissa.forfitt@10.4.193.20>
2018-03-07 11:23:59 +0000

    Put code into separate methods.

--- src/Encryption.java
+++ src/Encryption.java
@@ -2,32 +2,51 @@ import java.util.Scanner;
 
 public class Encryption {
 
-	public static void main(String[] args) {
-
-		String plaintext;
-		int key;
-		int cipher;
-		int plain;
-		String ciphertext = "";
-
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter the plaintext:");
-		plaintext = scan.nextLine();
-		System.out.println("Enter the key:");
-		key = scan.nextInt();
+	static int cipher;
+	static int plain;
+	static String ciphertext = "";
+	static String plaintext = ""; 
+	
+	public static String encrypt(String plaintext, int key) {
 
 		// Encrypts the word using Caesar cipher
 		for (int i = 0; i < plaintext.length(); i++) {
 			cipher = (int) plaintext.charAt(i) - (int) 'a' + key;
 			ciphertext = ciphertext + String.valueOf((char) (cipher + 64));
 		}
-		System.out.println("Encrypted word is: " + ciphertext);
+
+		return ciphertext;
+
+	}
+
+	public static String decrypt(String ciphertext, int key) {
 
 		// Decrypts the word using Caesar cipher
 		for (int i = 0; i < ciphertext.length(); i++) {
 			plain = (int) ciphertext.charAt(i) - (int) 'a' - key;
 			plaintext = plaintext + String.valueOf((char) (plain + 64));
 		}
+
+		return plaintext;
+
+	}
+
+	public static void main(String[] args) {
+
+		String plaintext;
+		String ciphertext;
+		int key;
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the plaintext:");
+		plaintext = scan.nextLine();
+		System.out.println("Enter the key:");
+		key = scan.nextInt();
+
+		ciphertext = encrypt(plaintext, key);
+		System.out.println("Encrypted word is: " + ciphertext);
+		
+		plaintext = decrypt(ciphertext, key);
 		System.out.println("Decrypted word is: " + plaintext);
 
 		scan.close();
